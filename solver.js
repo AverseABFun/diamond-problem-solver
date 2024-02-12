17723f8a7af61d73aba3243793bddbbb95120516
Arthur Beck <averse.abfun@gmail.com>
2024-02-11 18:15:16 -0600

    Update solver.js

--- solver.js
+++ solver.js
@@ -1,32 +1,44 @@
-const topNum = 12;
-const bottomNum = -7;
+const topNum = process.argv[1];
+const bottomNum = process.argv[2];
 
 if (topNum>0) {
-    for (var i = 0; i<topNum; i++) {
-        if (((topNum/i)%1 == 0) && i != 0) {
+    for (var i = 0; i<=topNum; i++) {
+        if (((topNum/i)||0)%1 == 0) {
             if (((topNum/i)+i)==bottomNum) {
+                console.log(`the answer is ${i} and ${topNum/i}`)
+            }
+            if (((topNum/-i)+i)==bottomNum) {
                 console.log(`the answer is ${i} and ${topNum/i}`)
             }
         }
     }
-    for (var i = 0; i>-topNum; i--) {
-        if (((topNum/i)%1 == 0) && i != 0) {
+    for (var i = 0; i>=-topNum; i--) {
+        if (((topNum/i)||0) == 0) {
             if (((topNum/i)+i)==bottomNum) {
+                console.log(`the answer is ${i} and ${topNum/i}`)
+            }
+            if (((topNum/-i)+i)==bottomNum) {
                 console.log(`the answer is ${i} and ${topNum/i}`)
             }
         }
     }
 } else {
-    for (var i = 0; i>topNum; i--) {
-        if (((topNum/i)%1 == 0) && i != 0) {
+    for (var i = 0; i>=topNum; i--) {
+        if (((topNum/i)||0)%1 == 0) {
             if (((topNum/i)+i)==bottomNum) {
+                console.log(`the answer is ${i} and ${topNum/i}`)
+            }
+            if (((topNum/-i)+i)==bottomNum) {
                 console.log(`the answer is ${i} and ${topNum/i}`)
             }
         }
     }
-    for (var i = 0; i<-topNum; i++) {
-        if (((topNum/i)%1 == 0) && i != 0) {
+    for (var i = 0; i<=-topNum; i++) {
+        if (((topNum/i)||0)%1 == 0) {
             if (((topNum/i)+i)==bottomNum) {
+                console.log(`the answer is ${i} and ${topNum/i}`)
+            }
+            if (((topNum/-i)+i)==bottomNum) {
                 console.log(`the answer is ${i} and ${topNum/i}`)
             }
         }
